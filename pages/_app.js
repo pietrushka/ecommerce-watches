8ee6deb31dd9ceccc09fe198e463f4d52db1df7f
pietrushka <piotrekwisnia18@gmail.com>
2020-09-16 20:17:07 +0200

    make overlay cart

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,53 @@ import '../styles/index.css'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
+
   const toggleHamburger = () => {
     document.querySelector('#overlay-menu').classList.toggle('translate-y-full')
   }
 
+  const toggleCart = () => {
+    document.querySelector('#overlay-cart').classList.toggle('hidden')
+  }
+
   return (
-    <div className='bg-secondary'>
+    <div className='relative bg-secondary'>
+
+      <div id='overlay-cart' className='fixed top-0 z-50 flex items-end justify-center hidden w-screen h-screen bg-black bg-opacity-50'>
+
+        <div id='cart-inner' className='w-11/12 mb-4 bg-white rounded-lg'>
+
+          <div className='relative my-1'>
+            <h2 className='text-4xl text-center'>Cart</h2>
+            <button onClick={toggleCart} className='absolute top-0 right-0 w-1/6 focus:outline-none'>
+              <svg className='ml-auto ' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+
+          </div>
+
+
+          <div className='flex items-center justify-center'>
+            <img className='inline-block object-cover w-1/3' src='https://wkruk.pl/product_picture/square_1024/55a0661afdb58cf2a4076feb7e4b09e3.png' />
+
+            <span className='w-1/3 text-center'>Omega</span>
+
+            <span className='w-1/3 text-center right-4'>$396</span>
+          </div>
+          <p className='text-center'>Swipe item to remove it</p>
+          <div className='flex items-center justify-center w-full my-2 rounded-t-lg '>
+            <button className='px-20 py-4 text-lg text-white rounded-full shadow-lg bg-primary focus:outline-none'>Checkout $395</button>
+          </div>
+        </div>
+      </div>
       
       <nav className='sticky top-0 z-30 w-full px-4 py-2 bg-white rounded-b-lg shadow text-primary' >
         <div className='flex items-center justify-between py-1'>
 
-          <button onClick={toggleHamburger} class="px-4 cursor-pointer md:hidden focus:outline-none" id='burger'>
-            <svg class="w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          <button onClick={toggleHamburger} className="px-4 cursor-pointer md:hidden focus:outline-none" id='burger'>
+            <svg className="w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
 
@@ -24,11 +58,11 @@ function MyApp({ Component, pageProps }) {
             </a>
           </Link>
           
-          <div class="px-4 cursor-pointer md:hidden" id='burger'>
-            <svg class='w-8' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+          <button onClick={toggleCart} className="px-4 cursor-pointer md:hidden focus:outline-none" id='burger'>
+            <svg className='w-8' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
             </svg>
-          </div>
+          </button>
         
           
         </div>
@@ -45,7 +79,7 @@ function MyApp({ Component, pageProps }) {
         </ul>
       </div>
 
-      <Component classList='min-h-screen' {...pageProps} />
+      <Component className='min-h-screen' {...pageProps} />
     </div>
   ) 
 }
--- pages/index.js
+++ pages/index.js
@@ -38,19 +38,19 @@ export default function Home() {
               
             <Link href='/product/seiko'>
               <a>
-                <h1 className='block text-lg text-2xl cursor-pointer'>Seiko Pepsi</h1>
+                <h1 className='block text-2xl cursor-pointer'>Seiko Pepsi</h1>
               </a>
             </Link>
 
 
-            <button className='inline-block w-6 h-6 my-2 mr-4 bg-blue-700 rounded-full cursor-pointer'></button>
-            <button className='inline-block w-6 h-6 my-2 mr-4 bg-black rounded-full cursor-pointer'></button>
-            <button className='inline-block w-6 h-6 my-2 mr-4 bg-gray-600 rounded-full cursor-pointer'></button>
+            <button className='inline-block w-6 h-6 my-2 mr-4 bg-blue-700 rounded-full cursor-pointer focus:outline-none'></button>
+            <button className='inline-block w-6 h-6 my-2 mr-4 bg-black rounded-full cursor-pointer focus:outline-none'></button>
+            <button className='inline-block w-6 h-6 my-2 mr-4 bg-gray-600 rounded-full cursor-pointer focus:outline-none'></button>
 
             <span className='float-right pr-1 my-1 text-lg align-text-top'>$ 395</span>
             
             <svg className='absolute top-0 right-0 w-10 mt-2 mr-2 cursor-pointer hover:text-red-600' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
           </div>
 
@@ -66,7 +66,7 @@ export default function Home() {
       
         <div className='bg-white rounded-lg shadow-lg'>
           <div className='relative p-2'>
-            <Link href='/product/seiko'>
+            <Link href='/product/omega'>
               <a>
                 <h2 className='cursor-pointer'>SRPD53K1</h2>
               </a>
@@ -74,17 +74,19 @@ export default function Home() {
               
             <Link href='/product/seiko'>
               <a>
-                <h1 className='block text-lg text-2xl cursor-pointer'>Seiko Pepsi</h1>
+                <h1 className='block text-2xl cursor-pointer'>Seiko Pepsi</h1>
               </a>
             </Link>
 
 
-            <button className='inline-block w-6 h-6 my-2 mr-4 bg-blue-700 rounded-full cursor-pointer'></button>
-            <button className='inline-block w-6 h-6 my-2 mr-4 bg-black rounded-full cursor-pointer'></button>
-            <button className='inline-block w-6 h-6 my-2 mr-4 bg-gray-600 rounded-full cursor-pointer'></button>
+            <button className='inline-block w-6 h-6 my-2 mr-4 bg-blue-700 rounded-full cursor-pointer focus:outline-none'></button>
+            <button className='inline-block w-6 h-6 my-2 mr-4 bg-black rounded-full cursor-pointer focus:outline-none'></button>
+            <button className='inline-block w-6 h-6 my-2 mr-4 bg-gray-600 rounded-full cursor-pointer focus:outline-none'></button>
+
+            <span className='float-right pr-1 my-1 text-lg align-text-top'>$ 395</span>
             
-            <svg className='absolute top-0 right-0 w-10 mt-2 mr-2 cursor-pointer' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+            <svg className='absolute top-0 right-0 w-10 mt-2 mr-2 cursor-pointer hover:text-red-600' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
           </div>
 
@@ -92,7 +94,7 @@ export default function Home() {
 
             <Link href='/product/seiko'>
               <a>
-                <img className='' src='https://salonkwadrans.pl/wp-content/uploads/2019/11/Seiko-SRPD53K1-5-Sports-Automatic.png' />
+                <img className='' src='https://wkruk.pl/product_picture/square_1024/55a0661afdb58cf2a4076feb7e4b09e3.png' />
               </a>
             </Link>
           </div>
