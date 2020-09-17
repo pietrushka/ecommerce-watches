d63bdf2cc2ad3b60884826663f92672aa0d47bde
pietrushka <piotrekwisnia18@gmail.com>
2020-09-17 09:54:34 +0200

    Add cart animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,50 @@
 import '../styles/index.css'
 import Link from 'next/link'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  let overlayBg, cart
+  useEffect(() => {
+    overlayBg = document.querySelector('#overlayBg')
+    cart = document.querySelector('#cart')
+  }, [])
 
   const toggleHamburger = () => {
     document.querySelector('#overlay-menu').classList.toggle('translate-y-full')
   }
 
-  const toggleCart = () => {
-    document.querySelector('#overlay-cart').classList.toggle('hidden')
+
+  // toggle doesnt't applay transition when cart is hidding 
+  const showCart = () => {
+    overlayBg.classList.remove('hidden')
+    cart.classList.remove('hidden')
+    setTimeout(() => {
+      cart.classList.remove('translate-y-full')
+    }, 100)
+  }
+
+  const hideCart = () => {
+   cart.classList.add('translate-y-full')
+    setTimeout(() => {
+      cart.classList.add('hidden')
+      overlayBg.classList.add('hidden')
+    }, 100)
   }
 
   return (
     <div className='relative bg-secondary'>
 
-      <div id='overlay-cart' className='fixed top-0 z-50 flex items-end justify-center hidden w-screen h-screen bg-black bg-opacity-50'>
+      <div id='overlayBg' onClick={hideCart} className='fixed top-0 z-40 flex items-end justify-center hidden w-screen h-screen bg-black bg-opacity-50'></div>
 
-        <div id='cart-inner' className='w-11/12 mb-4 bg-white rounded-lg'>
-
-          <div className='relative my-1'>
-            <h2 className='text-4xl text-center'>Cart</h2>
-            <button onClick={toggleCart} className='absolute top-0 right-0 w-1/6 focus:outline-none'>
-              <svg className='ml-auto ' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-
-          </div>
+      <div id='cart' className='fixed bottom-0 z-50 hidden w-11/12 m-4 transition-transform duration-500 ease-out transform translate-y-full bg-white rounded-lg'>
+        <div className='relative my-1'>
+          <h2 className='text-4xl text-center'>Cart</h2>
+          <button onClick={hideCart} className='absolute top-0 right-0 w-1/6 focus:outline-none'>
+            <svg className='ml-auto ' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
 
 
           <div className='flex items-center justify-center'>
@@ -41,7 +59,6 @@ function MyApp({ Component, pageProps }) {
             <button className='px-20 py-4 text-lg text-white rounded-full shadow-lg bg-primary focus:outline-none'>Checkout $395</button>
           </div>
         </div>
-      </div>
       
       <nav className='sticky top-0 z-30 w-full px-4 py-2 bg-white rounded-b-lg shadow text-primary' >
         <div className='flex items-center justify-between py-1'>
@@ -53,12 +70,12 @@ function MyApp({ Component, pageProps }) {
           </button>
 
           <Link href='/'>
-            <a>
+            <a className='focus:outline-none'> 
               <h1 className='p-4 text-xl font-bold'>Sikory.</h1>
             </a>
           </Link>
           
-          <button onClick={toggleCart} className="px-4 cursor-pointer md:hidden focus:outline-none" id='burger'>
+          <button onClick={showCart} className="px-4 cursor-pointer md:hidden focus:outline-none" id='burger'>
             <svg className='w-8' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
             </svg>
