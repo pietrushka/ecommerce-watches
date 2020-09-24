7bc74d0962a5857f00becb004242a480065d71cb
pietrushka <piotrekwisnia18@gmail.com>
2020-09-24 14:01:37 +0200

    Change addImageUrl function to consume cloudinary urls

--- pages/_app.js
+++ pages/_app.js
@@ -13,30 +13,30 @@ export default function App ({ Component, pageProps }) {
 
   const providerValue = useMemo(() => ({ user, setUser, isAuthenticated: !!user }), [user, setUser])
 
-  // useEffect(() => {
-  //   // grab token value from cookie
-  //   const token = Cookies.get('token')
+  useEffect(() => {
+    // grab token value from cookie
+    const token = Cookies.get('token')
 
-  //   if (token) {
-  //   // authenticate the token on the server and place set user object
-  //     axios.get(`${process.env.API_URL}/users/me`, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`
-  //       }
-  //     }).then(async (res) => {
-  //       console.log(res)
-  //       // if res comes back not valid, token is not valid
-  //       // delete the token and log the user out on client
-  //       if (res.statusText !== 'OK') {
-  //         Cookies.remove('token')
-  //         setUser(null)
-  //         return null
-  //       }
-  //       const user = res.data
-  //       setUser(user)
-  //     })
-  //   }
-  // }, [])
+    if (token) {
+    // authenticate the token on the server and place set user object
+      axios.get(`${process.env.API_URL}/users/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then(async (res) => {
+        console.log(res)
+        // if res comes back not valid, token is not valid
+        // delete the token and log the user out on client
+        if (res.statusText !== 'OK') {
+          Cookies.remove('token')
+          setUser(null)
+          return null
+        }
+        const user = res.data
+        setUser(user)
+      })
+    }
+  }, [])
 
   return (
     <AppContext.Provider value={providerValue}>
