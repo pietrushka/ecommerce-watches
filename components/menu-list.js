c250a02e674cf4d15d18bc990117f36f7a3c7cf4
pietrushka <piotrekwisnia18@gmail.com>
2020-10-06 17:51:22 +0200

    Add current page indicator

--- components/menu-list.js
+++ components/menu-list.js
@@ -1,16 +1,40 @@
 import { useContext } from 'react'
 import Link from 'next/link'
-import Router from 'next/router'
+import { useRouter } from 'next/router'
 import Cookies from 'js-cookie'
 
 import AppContext from '../context/app-context'
 import { useCart } from '../hooks/useCart'
 
 export default function MenuList ({ orientation, closeMenu }) {
+  const Router = useRouter()
   const { user, setUser } = useContext(AppContext)
   const { clearCart } = useCart()
+  const pathName = Router.pathname.slice(1)
+  console.log(pathName)
 
-  const liStyles = orientation === 'horizontal' ? ' text-xl font-bold px-6 relative addMenuUnderlineOnHover' : 'px-6 py-2 text-center bg-primary text-secondary my-5 rounded-full border-2 border-primary'
+  const liStyles = optionName => {
+    if (orientation === 'horizontal') {
+      let horizontalStyles = 'text-lg font-bold px-6 relative'
+
+      if (optionName === pathName) {
+        horizontalStyles = `${horizontalStyles} addFixedUnderline`
+      } else {
+        horizontalStyles = `${horizontalStyles} addMenuUnderlineOnHover`
+      }
+
+      return horizontalStyles
+    } else {
+      let verticalStyles = 'px-6 py-2 text-center my-5 rounded-full border-2 border-primary'
+      const invertStyles = 'bg-primary text-secondary'
+      if (optionName === pathName) {
+        verticalStyles = `${verticalStyles} ${invertStyles}`
+      } else {
+        verticalStyles = `${verticalStyles} bg-secondary text-primary hover:bg-primary hover:text-secondary`
+      }
+      return verticalStyles
+    }
+  }
 
   const ulClassNames = orientation === 'horizontal'
     ? 'hidden md:flex lg:text-xl xl:text-2xl'
@@ -31,37 +55,37 @@ export default function MenuList ({ orientation, closeMenu }) {
   return (
 
     <ul className={` text-center ${ulClassNames} `}>
-      <li className={liStyles} onClick={closeMenu}>
+      <li className={liStyles('favorites')} onClick={closeMenu}>
         <Link href='/favorites'>
-          <a>Favorites</a>
+          <a className='outline-none'>Favorites</a>
         </Link>
       </li>
-      <li className={liStyles} onClick={closeMenu}>
+      <li className={liStyles('orders')} onClick={closeMenu}>
         <Link href='/orders'>
-          <a>Orders</a>
+          <a className='outline-none'>Orders</a>
         </Link>
       </li>
-      <li className={liStyles} onClick={closeMenu}>
+      <li className={liStyles('contact')} onClick={closeMenu}>
         <Link href='/contact'>
-          <a>Contact</a>
+          <a className='outline-none'>Contact</a>
         </Link>
       </li>
       {user ? (
-        <li className={liStyles} onClick={closeMenu}>
-          <button onClick={logout}>
-                Logout
+        <li className={liStyles('logout')} onClick={closeMenu}>
+          <button className='outline-none' onClick={logout}>
+            Logout
           </button>
         </li>
       ) : (
         <>
-          <li className={liStyles} onClick={closeMenu}>
+          <li className={liStyles('register')} onClick={closeMenu}>
             <Link href='/register'>
-              <a>Register</a>
+              <a className='outline-none'>Register</a>
             </Link>
           </li>
-          <li className={liStyles} onClick={closeMenu}>
+          <li className={liStyles('login')} onClick={closeMenu}>
             <Link href='/login'>
-              <a>Login</a>
+              <a className='outline-none'>Login</a>
             </Link>
           </li>
         </>
